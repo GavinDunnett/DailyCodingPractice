ab246790312b2bc0e098b29d7490a9818fc0514b
Gavin Dunnett <gavin.dunnett@gmail.com>
2021-02-27 11:53:03 -0600

    refactor day4

--- src/Day0004.java
+++ src/Day0004.java
@@ -4,17 +4,15 @@ import java.util.HashMap; // package required for map data structure.
 /**
  * This class is coding practice from codingbat.com
  * <p>
- * Preamble:
- * Maps are a great data structure for taking in large amounts of data and
- * giving super-fast access. A map stores key/value pairs, where each key has
- * an associate value. Given a particular key, the map can look up the
+ * Preamble: Maps are a great data structure for taking in large amounts of data
+ * and giving super-fast access. A map stores key/value pairs, where each key
+ * has an associate value. Given a particular key, the map can look up the
  * associated value very quickly.
  * <p>
- * * word0 Challenge:
- * Given an array of strings, return a Map<String, Integer> containing a
- * key for every different string in the array, always with the value 0.
- * For example the string "hello" makes the pair "hello":0. We'll do more
- * complicated counting later, but for this problem the value is simply 0.
+ * * word0 Challenge: Given an array of strings, return a Map<String, Integer>
+ * containing a key for every different string in the array, always with the
+ * value 0. For example the string "hello" makes the pair "hello":0. We'll do
+ * more complicated counting later, but for this problem the value is simply 0.
  *
  * @author Gavin Dunnett
  * @version 1.0
@@ -32,11 +30,11 @@ public class Day0004 {
 		HashMap<String, Integer> strMap = new HashMap<>();
 
 		// array holding strings.
-		String[] strs = {"Alice", "Bob", "Clive", "David", "Fred", "Alice"};
+		String[] strs = { "Alice", "Bob", "Clive", "David", "Fred", "Alice" };
 
 		// enhanced for loop.
 		for (String str : strs) {
-			//check is string is already a key, if not add it.
+			// check is string is already a key, if not add it.
 			if (!strMap.containsKey(str)) { // check if map already has string.
 				strMap.put(str, 0); // add string to map.
 			}
